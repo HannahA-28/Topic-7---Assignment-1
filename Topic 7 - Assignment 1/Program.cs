78be5b46cdc086b15831c327a80fc350af3461b4
Hannah Anderson <339729345@GoTVDSB.ca>
2025-03-31 14:09:52 -0400

    Add project files.

---
+++ Topic 7 - Assignment 1/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Topic_7___Assignment_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
